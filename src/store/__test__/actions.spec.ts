dcd79145b2a357a969a1be03e8f67c8a4c703585
Aida Fuentes <afdlf236@gmail.com>
2025-03-09 21:07:58 +0100

    test mockeado

--- src/store/__test__/actions.spec.ts
+++ src/store/__test__/actions.spec.ts
@@ -1,6 +1,7 @@
-import { describe, test, expect } from "vitest";
 import type { Advert } from "@/pages/adverts/types";
-import { AdvertsLoadedFulfilled, AuthLogout } from "../actions";
+import { AdvertsLoadedFulfilled, authLogin, AuthLogout } from "../actions";
+import type { Credentials } from "@/pages/auth/types";
+import { ApiClientError } from "@/api/error";
 
 //Accion sincrona
 
@@ -35,5 +36,50 @@ describe("AdvertsLoadedFulfilled", () => {
     const result = AdvertsLoadedFulfilled(adverts);
     expect(result).toEqual(action);
   });
+});
+
+
+
+//Comprobar el funcionamiento de un componente que ejecuta una acción del store, mockeando la acción
+describe("authLogin", () => {
+  afterEach(() => {
+    dispatch.mockClear();
+  });
+
+  const credentials: Credentials = {
+    email: "admin@gmail.com",
+    password: "password",
+  };
+  const remember = false;
+  const dispatch = vi.fn();
+  const thunk = authLogin(credentials, remember);
+
+  let api = {
+    login: vi.fn(),
+  };
+
+  test("when login resolves", async () => {
+    api.login.mockResolvedValue(undefined);
+    //@ts-expect-error: getState not used
+    await thunk(dispatch, undefined, { login: api.login });
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: "auth/login/pending" });
+    //expect(api.login).toHaveBeenCalledWith(credentials, remember);
+    //expect(dispatch).toHaveBeenNthCalledWith(2, { type: "auth/login/fulfilled" });
+  });
+
+  test("when login rejects", async () => {
+    api.login.mockRejectedValue(new ApiClientError("Unauthorized"));
+
+    //@ts-expect-error: getState not used
+    await thunk(dispatch, undefined, { login: api.login });
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, { type: "auth/login/pending" });
+    //expect(api.login).toHaveBeenCalledWith(credentials, remember);
+    //expect(dispatch).toHaveBeenNthCalledWith(2, { type: "auth/login/rejected", payload: new ApiClientError("Unauthorized") });
+  });
+
 });
 
